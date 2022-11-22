e00040e79394d4a05b49e675981e88269b78d9e1
doraaan89 <doraaan89@gmail.com>
2022-11-22 23:07:34 +0900

    firestore test

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import { app, db } from '../firebase';
 
 Vue.config.productionTip = false;
+Vue.prototype.$firebase = app;
+Vue.prototype.$firestore = db;
 
 new Vue({
 	router,
