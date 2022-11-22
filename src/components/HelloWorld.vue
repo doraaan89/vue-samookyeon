3a9a73b061f8a8c290b6d18731386822b029a469
doraaan89 <doraaan89@gmail.com>
2022-11-23 00:32:12 +0900

    card img sample

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,68 @@
 <template>
-	<v-container> 여기에서 사용하고 싶은 내용을 알려주세요. </v-container>
+	<v-container>
+		<v-card
+			v-for="(item, i) in sampleData"
+			:key="i"
+			class="mx-auto mt-4"
+			max-width="400"
+		>
+			<v-img class="white--text align-end" height="200px" :src="item.src">
+				<v-card-title>{{ item.imgTitle }}</v-card-title>
+			</v-img>
+
+			<v-card-subtitle class="pb-0">{{ item.date }}</v-card-subtitle>
+
+			<v-card-text class="text--primary">
+				<div>{{ item.title }}</div>
+
+				<div>{{ item.text1 }}</div>
+			</v-card-text>
+
+			<v-card-actions>
+				<v-btn color="orange" text> 공유 </v-btn>
+
+				<v-btn color="orange" text> 탐험이유 </v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-container>
 </template>
 
 <script>
 export default {
 	name: 'HelloWorld',
-
-	data: () => ({}),
+	data() {
+		return {
+			sampleData: [
+				{
+					src: require('@/assets/images/1.jpeg'),
+					imgTitle: '이거유',
+					date: new Date(),
+					title: '이거유',
+					text: '이거유',
+				},
+				{
+					src: require('@/assets/images/2.jpeg'),
+					imgTitle: '이거유',
+					date: new Date(),
+					title: '이거유',
+					text: '이거유',
+				},
+				{
+					src: require('@/assets/images/3.jpeg'),
+					imgTitle: '이거유',
+					date: new Date(),
+					title: '이거유',
+					text: '이거유',
+				},
+				{
+					src: require('@/assets/images/4.jpeg'),
+					imgTitle: '이거유',
+					date: new Date(),
+					title: '이거유',
+					text: '이거유',
+				},
+			],
+		};
+	},
 };
 </script>
